7a10b1e001b0ac6b70745be704122482c22ad09e
Kofi Agyepong <agyepongkofi123@gmail.com>
2024-05-10 23:19:17 -0400

    Modified code to use Kevin's TDR

--- nano_vna.py
+++ nano_vna.py
@@ -1,38 +1,20 @@
 # import the modules
 import matplotlib.pyplot as plt
 import skrf
-from skrf import Frequency
+from scipy import signal
 from skrf.vi.vna import nanovna
 from skrf.calibration import OnePort
-from serial.tools import list_ports
-from scipy import signal
+from nano_vna_funcs import *
 
 c = 3e8 # speed of light [m/s]
 v_f = 1 # velocity factor
-
-def ripData():
-    # Returns (x_data, y_data) of the function currently plotted in pyplot
-    plot = (plt.gca()).lines[0]
-    x_data = plot.get_data()[0]
-    y_data = plot.get_data()[1]
-    return x_data, y_data
-
-# Get nanovna device automatically
-# https://github.com/nanovna-v2/NanoVNA2-firmware/blob/master/python/nanovna.py
-VIDPIDs = set([(0x0483, 0x5740), (0x04b4,0x0008)]);
-def getport() -> str:
-    device_list = list_ports.comports()
-    for device in device_list:
-        if (device.vid, device.pid) in VIDPIDs:
-            return device.device
-    raise OSError("device not found")
 
 COM_Port = getport()[3] # get rid of 'COM' before the com port number
 instr = nanovna.NanoVNA('ASRL'+COM_Port+'::INSTR')
 # Assign frequency range and number of points
 instr.freq_start = 2.4e9
 instr.freq_stop = 2.4e9 + 150e6
-instr.npoints = 15000
+instr.npoints = 1000
 
 # Calibrate
 input("Please connect a SHORT to port 1 at\nthe reference plane, then press enter:\n")
@@ -57,22 +39,6 @@ s11, _ = instr.get_s11_s21()
 # apply the calibration
 s11_cal = cal.apply_cal(s11)
 
-# plot the calibrated s11
-s11_cal.plot_s_db()
-plt.title("s_db freq")
-plt.show()
-
 # plot calibrated s11 in time domain
-s11_cal.plot_s_db_time()
-plt.title("Calibrated db vs time")
-x_data, y_data = ripData()
-#print(f"x_data: {x_data}")
-#print(f"y_data: {y_data}")
-peak_inds, _ = signal.find_peaks(y_data, height=-3)
-peak_vals = [y_data[ind] for ind in peak_inds]
-print(f"peak inds: {peak_inds}\npeak data: {peak_vals}")
-plt.show()
-
-#s11_cal.plot_s_time_mag()
-#plt.title("Calibrated mag vs time")
-#plt.show()
+data_plot, _ = measureTDR(s11_cal, instr.freq_step)
+plt.plot(data_plot)
---
+++ nano_vna_funcs.py
@@ -0,0 +1,79 @@
+# import the modules
+import numpy as np
+import scipy
+from serial.tools import list_ports
+
+# Get nanovna device automatically
+# https://github.com/nanovna-v2/NanoVNA2-firmware/blob/master/python/nanovna.py
+VIDPIDs = set([(0x0483, 0x5740), (0x04b4,0x0008)]);
+def getport() -> str:
+    device_list = list_ports.comports()
+    for device in device_list:
+        if (device.vid, device.pid) in VIDPIDs:
+            return device.device
+    raise OSError("device not found")
+
+def extrapolatetodc(f,s):
+    """Extrapolate s parameters to DC. Assumes the frequency difference between
+    DC and the first point is equal to the frequency difference between adjacent
+    points"""
+    fwithDC = np.insert(f,0,0)
+    swithDC = np.insert(s,0,0)
+    phase = scipy.interpolate.interp1d(f, np.unwrap(np.angle(s)), axis=0,
+    fill_value='extrapolate')(0)
+    magnitude = scipy.interpolate.interp1d(f, np.abs(s), axis=0,
+    fill_value='extrapolate')(0)
+    swithDC[0] = np.real(magnitude*np.exp(-1j*phase))
+    return fwithDC,swithDC
+
+def calculateTDR(f,s,mode='lowpass_step',window='normal',electrical_delay=0.0):
+    """Calculate the time domain step response from the frequency domain data.
+    Uses the same window as the NanoVNA v2 firmware which can be found at
+    https://github.com/nanovna-v2/NanoVNA-V2-firmware in the file main2.cpp
+    in the function transform_domain() with option TD_FUNC_LOWPASS_STEP"""
+    points = len(s)
+    #the window is symmetric about the middle of the array
+    #if mode is lowpass, made window twice and wide and apply only
+    #the right half of the window to the data.
+    if mode == 'lowpass_step' or mode == 'lowpass_impulse':
+        window_size = 2*points
+    else:
+        window_size = points
+    #apply a window to reduce high frequency noise and aliasing
+    #use a kaiser window for consistency with NanoVNA firmware
+    #beta = 6 for TD_WINDOW_NORMAL, 0 for TD_WINDOW_MINIMUM, and 13 for TD_WINDOW_MAXIMUM
+    if window == 'minimum':
+        beta = 0 #rectangular window in the frequency domain
+    elif window == 'normal':
+        beta = 6
+    elif window == 'maximum':
+        beta = 13
+    else:
+        raise Exception("Allowed window sizes are minimum, normal, or maximum")
+    window_points = np.kaiser(window_size,beta)
+    #if we are using one of the lowpass modes, apply only the right half
+    #of the window to the data. otherwise apply the full window.
+    if mode == 'lowpass_step' or mode == 'lowpass_impulse':
+        snoDC = s*window_points[points:]
+    else:
+        snoDC = s*window_points
+    #extrapolate the s parameters to DC
+    fwithDC, swithDC = extrapolatetodc(f,snoDC)
+    #we expect a real signal but we are only measuring positive frequencies.
+    #append the complex conjugate (without the dc component to preserve conjugate symmetry)
+    swithconj = np.concatenate((swithDC,np.conj(np.flip(snoDC))))
+    ## same as above but zero padding
+    # pad_width=1024-(2*points+1)
+    # swithconj = np.concatenate((np.pad(swithDC,(0,pad_width)),np.pad(np.conj(np.flip(snoDC)),(pad_width,0))))
+    #inverse fourier transform to get back into the time domain
+    td = np.fft.ifft(swithconj)
+    #take the cumulative sum to simulate the step response (rather than the impulse response)
+    if mode == 'lowpass_step':
+        td = np.cumsum(td)
+        #calculate the time axis
+        tmax = 1 / (f[1] - f[0])
+        t = np.linspace(0, tmax, len(td))
+    return t,td
+
+def measureTDR(s11, f, mode='lowpass_step',window='normal',electrical_delay=0.0):
+    return calculateTDR(f,s11,mode,window,electrical_delay)
